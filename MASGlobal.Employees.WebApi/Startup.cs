0ccff49553b8c24495354c75b05983406a3e27b3
Carlos Carrero <carlos.carrero@gmail.com>
2019-06-08 00:49:42 -0500

    Adding swagger

--- MASGlobal.Employees.WebApi/Startup.cs
+++ MASGlobal.Employees.WebApi/Startup.cs
@@ -1,10 +1,18 @@
-﻿using AutoMapper;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using AutoMapper;
+using MASGlobal.Employees.Data.Contracts;
+using MASGlobal.Employees.Data.Repositories;
+using MASGlobal.Employees.Rest.Contracts;
+using MASGlobal.Employees.Rest.Implementations;
 using MASGlobal.Employees.WebApi.Mappings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace MASGlobal.Employees.WebApi
 {
@@ -19,13 +27,29 @@ namespace MASGlobal.Employees.WebApi
 
         public void ConfigureServices(IServiceCollection services)
         {
-            var mappingConfig = new MapperConfiguration(mc =>
-            {
-                mc.AddProfile(new MappingProfile());
-            });
+            var mappingConfig = new MapperConfiguration(mc => { mc.AddProfile(new MappingProfile()); });
 
             var mapper = mappingConfig.CreateMapper();
             services.AddSingleton(mapper);
+
+            services.AddScoped<IRestClient, RestClient>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Info
+                {
+                    Title = "Employees Web API",
+                    Version = "v1",
+                    Description = "API to manage employees"
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+
+                options.CustomSchemaIds(type => type.FullName);
+            });
 
             services.AddMvc();
 
@@ -38,6 +62,14 @@ namespace MASGlobal.Employees.WebApi
                 app.UseDeveloperExceptionPage();
             else
                 app.UseHsts();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Employees Web API");
+                options.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
